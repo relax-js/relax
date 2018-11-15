43647cb68a1d352cf714aacedd2db963e9312f5c
Jason McNeal <jmcneal@overstock.com>
2018-11-14 20:54:27 -0700

    Better handling for action returning undefined

--- src/dispatch.js
+++ src/dispatch.js
@@ -1,8 +1,16 @@
 import { actionParams, composeAction } from './composeAction';
 
+function buildResponse(store, result) {
+    return Object.assign({}, actionParams(store), { result });
+}
+
+function defaultResponse(store, result) {
+    return Promise.resolve(buildResponse(store, result));
+}
+
 function resolveAction(store, result) {
     // For nested dispatch calls - only return result
-    if (!result || result.dispatch) return result;
+    if (!result || result.dispatch) return defaultResponse(store, result);
 
     // Set new state - don't return _action
     const next = Object.assign({}, result);
@@ -10,9 +18,7 @@ function resolveAction(store, result) {
     store.setState(next);
 
     // Send result to all subscribers
-    const response = Object.assign({}, actionParams(store), { result });
-
-    // Send result to all subscribers
+    const response = buildResponse(store, result);
     store.callSubscribers(response);
 
     // Ensure to return promise for sync actions
@@ -22,7 +28,8 @@ function resolveAction(store, result) {
 export default function dispatch(store, action) {
     const result = composeAction(store, action);
 
-    if (!result) return result;
+    // If action returned undefined then drop out here but return default response
+    if (!result) return defaultResponse(store, result);
 
     // Check if Promise - return async
     if ('then' in result) return Promise.resolve(result).then(data => resolveAction(store, data));
